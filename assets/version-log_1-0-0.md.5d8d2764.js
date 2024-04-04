c13cb32b42ab4096207da046b0a5f60efddfd066
张彩聪 <zhangcc@topode.com>
2024-04-04 16:38:46 +0800

    fa123

---
+++ assets/version-log_1-0-0.md.5d8d2764.js
@@ -0,0 +1,9 @@
+import{_ as e,c as a,o as d,Q as t}from"./chunks/framework.a87c977f.js";const b=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"version-log/1-0-0.md","filePath":"version-log/1-0-0.md","lastUpdated":1709880088000}'),o={name:"version-log/1-0-0.md"},i=t(`<h2 id="_1-0-0-beta-5" tabindex="-1">1.0.0-beta-5</h2><h4 id="一、upload组件调整" tabindex="-1">一、upload组件调整</h4><h5 id="_1、urimageupload-vue-图片上传功能" tabindex="-1">1、UrImageUpload.vue 图片上传功能</h5><pre><code>    feat: 添加fileUrl字段，支持新版OSS回显。取消内部code拼接功能。
+    feat: 添加 defaultImage 组件默认背景图片
+</code></pre><h5 id="_2、urcascader-vue-级联选择器" tabindex="-1">2、UrCascader.vue 级联选择器</h5><pre><code>    fix：修复change事件无法使用问题
+</code></pre><h2 id="_1-0-0-beta-3" tabindex="-1">1.0.0-beta-3</h2><h4 id="一、upload组件调整-1" tabindex="-1">一、upload组件调整</h4><h5 id="_1、urfileupload-vue-多文件上传功能" tabindex="-1">1、UrFileUpload.vue 多文件上传功能</h5><pre><code>    fix: 取消文件上传默认过期时间字段
+</code></pre><h2 id="_1-0-0-beta-2" tabindex="-1">1.0.0-beta-2</h2><h4 id="一、upload组件调整-2" tabindex="-1">一、upload组件调整</h4><h5 id="_1、urfileupload-vue-多文件上传功能-1" tabindex="-1">1、UrFileUpload.vue 多文件上传功能</h5><pre><code>    fix: 修复组件无法预览功能
+</code></pre><h2 id="_1-0-0-beta-1" tabindex="-1">1.0.0-beta-1</h2><h4 id="一、upload组件调整-3" tabindex="-1">一、upload组件调整</h4><h5 id="_1、支持oss图片、文件上传v2版本" tabindex="-1">1、支持OSS图片、文件上传V2版本</h5><pre><code>   + 调整OSS接口上传参数，将files[] =&gt; file 、 access_token =&gt; token
+   +  OSS文件上传接口返回字段变更，UrFileUpload.vue 、UrImageUpload.vue 
+      调整变更字段 file_code =&gt; code 、file_name=&gt; name
+</code></pre>`,18),n=[i];function r(l,_,c,p,h,s){return d(),a("div",null,n)}const f=e(o,[["render",r]]);export{b as __pageData,f as default};
